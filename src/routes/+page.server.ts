fd60cc72abb9a503e1c5a37475c7e3c614273ba2
Douglas A <andreani.dr@gmail.com>
2023-01-17 11:41:18 -0300

    feat(filter): add filter to main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,16 @@
 import type { PageServerLoad } from './$types';
 import type { Todos }  from '$lib/types/Todo';
 
-export const load = (async () => {
-    let url = 'https://dummyjson.com/todos';
-    let data = await fetch(url);
+export const load = (async ({ params, url }) => {
+    let todos_url = 'https://dummyjson.com/todos';
+    let data = await fetch(todos_url);
     let todos: Todos = await data.json();
 
+    let filter = url.searchParams.get('filter');
+
+
     return {
-        todos: todos.todos
+        todos: todos.todos, 
+        filter: filter,
     }
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,48 @@
 
     export let data: PageData;
 
+    let todos = data.todos;
+
+    $: todosref = "";
+
+    function update_todos() {
+        if (todosref) {
+            if (todosref === 'completed')
+                todos = data.todos.filter(todo => todo.completed);
+            else if (todosref === 'inprogress')
+                todos = data.todos.filter(todo => !todo.completed);
+            else 
+                todos = data.todos;
+        } else {
+            todos = data.todos;
+        }
+    }
+
+    function handleClick(e: any) {
+        if (e.target.id === "completed") {
+            todosref = "completed"
+        } else if (e.target.id === "inprogress") {
+            todosref = "inprogress"
+        } else {
+            todosref = "";
+        }
+
+        update_todos();
+    }
+
 </script>
+
+
 <div class='container p-4 justify'>
-{#each data.todos as todo}
+<button class="btn btn-primary" id="all" on:click={handleClick}>All</button>
+<button class="btn btn-primary" id="inprogress" on:click={handleClick}>In Progress</button>
+<button class="btn btn-primary" id="completed" on:click={handleClick}>Completed</button>
+
+<div class="grid md:grid-cols-3 sm:grid-cols-1 px-4" >
+{#each todos as todo}
     <div class="text-2 text-justify py-4">
         <Todo todo={todo}/>
     </div>
 {/each}
+</div>
 </div>
